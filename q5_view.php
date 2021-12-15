6d2a468e5e5b5764b17431eed581894a0cbee99d
sanzida2305 <92871762+sanzida2305@users.noreply.github.com>
2021-12-16 01:04:25 +0600

    Add files via upload

---
+++ q5_view.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+
+<?php
+if(!isset($_SESSION['valid'])) {
+    header('Location: login.php');
+}
+?>
+
+<?php
+include_once("connection.php");
+
+$result = mysqli_query($mysqli, "SELECT * FROM reg WHERE login_id=".$_SESSION['id']." ORDER BY id DESC");
+?>
+
+<html>
+<head>
+    <title>Homepage</title>
+</head>
+
+<body>
+<a href="index.php">Home</a> | <a href="add.html">Add New Data</a> | <a href="logout.php">Logout</a>
+<br/><br/>
+	
+<table width='80%' border=0>
+    <tr bgcolor='#CCCCCC'>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Phone (euro)</td>
+        
+    </tr>
+    <?php
+    while($res = mysqli_fetch_array($result)) {		
+        echo "<tr>";
+        echo "<td>".$res['name']."</td>";
+        echo "<td>".$res['eml']."</td>";
+        echo "<td>".$res['phn']."</td>";	
+        echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
+    }
+    ?>
+</table>	
+</body>
+</html>
